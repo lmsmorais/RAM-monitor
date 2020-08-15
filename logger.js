3d8803d3d68c9fba6cecfaa20635a9ad93733717
Luís Fernando Lima de Morais Assunção <luismorais@mba-luismorais.local>
2020-08-15 16:30:45 -0300

    add module fs and path in logger.js

--- logger.js
+++ logger.js
@@ -1,9 +1,13 @@
 const EventEmitter = require('events')
+const fs = require('fs')
+const path = require('path')
 
 const emitter = new EventEmitter()
 
 emitter.on('log', (message) => {
-    console.log(message)
+    fs.appendFile(path.join(__dirname, 'log.txt'),message, err => {
+        if (err) throw err
+    })
 })
 
 function log(message) {
