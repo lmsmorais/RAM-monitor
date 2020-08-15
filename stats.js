3b3c7a1aef1cc4e4b60ee0c4de5a70e09cf8795e
Luís Fernando Lima de Morais Assunção <luismorais@mba-luismorais.local>
2020-08-15 16:25:30 -0300

    importing the log function to stats.js

--- stats.js
+++ stats.js
@@ -1,4 +1,5 @@
 const os = require('os')
+const log = require('./logger')
 
 setInterval(() => {
     const { freemem, totalmem } = os
@@ -15,6 +16,8 @@ setInterval(() => {
     console.clear()
     console.log(" ====== PC STATS =====")
     console.table(stats)
+
+    log('Rodando...')
 
 },1000)
 
